9c06af53fc8a1a46fec14d91562cbabfeb3bc4d5
tejas.chauhan <tejas.chauhan@ezeetechnosys.com>
2023-10-30 19:34:19 +0530

    sadsad

--- git_commands.sh
+++ git_commands.sh
@@ -636,11 +636,15 @@ solveConflict() {
         echo -e "\n${CYAN}${BOLD}git commit -m 'Resolved conflicts'${NC}"
         logIt "git commit -m Resolved conflicts"
         git commit -m "Resolved conflicts"
-
+echo ${1}
+echo ${2}
+echo ${3}
         if [ $1 == "pull" ] ; then
-            mergeParaBranch $1 $2 $3
+        echo "asdsa"
+           # mergeParaBranch $1 $2 $3
         elif [ $1 == "merge" ] ; then
-            mergeBranchWithParent $2 $3
+           # mergeBranchWithParent $2 $3
+           echo "asdsaasdasdsadsa_Asdasdasdas_Asdsa"
         fi
     else
         echo -e "\n${RED}${BOLD}Operation aborted. Reverting changes.${NC}"
